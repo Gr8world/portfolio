c49a8023bf4abcc66e7851ede9293820066a3f77
Shoaib Akhtar <166367752+Gr8world@users.noreply.github.com>
2026-04-07 13:48:39 +0530

    Update settings.py

--- startup_site/settings.py
+++ startup_site/settings.py
@@ -22,7 +22,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-dev-key-change-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["aaizy.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 INSTALLED_APPS = [
